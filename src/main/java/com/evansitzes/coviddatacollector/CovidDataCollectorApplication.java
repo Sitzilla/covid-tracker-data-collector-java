235edc0ab41de5567a5fb2d3370496a4ec0cbe8d
Sitzilla <evan.sitzes@xpanse.com>
2022-01-10 16:33:51 -0800

    Initial commmit

---
+++ src/main/java/com/evansitzes/coviddatacollector/CovidDataCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.evansitzes.coviddatacollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidDataCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidDataCollectorApplication.class, args);
+    }
+
+}
